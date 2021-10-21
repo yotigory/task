3621f3b4b3322c3fbc1355dc8dc08eb13e573de3
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2021-10-21 19:01:27 +0900

    SET LoginPage

--- apps/task/src/main.tsx
+++ apps/task/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@galaxy/shifter/dist/css/styles.css';
+import { App } from './app/app';
 
-import App from './app/app';
 
 ReactDOM.render(
   <StrictMode>
