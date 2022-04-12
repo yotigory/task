06caef972ba8bc68ca822961b978400761b3d1ef
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2022-04-12 15:06:12 +0900

    noindex にする

--- apps/task/src/app/component/footer.tsx
+++ apps/task/src/app/component/footer.tsx
@@ -2,11 +2,8 @@ import { Copyright } from '@galaxy/shifter';
 
 export const Footer = () => {
   return (
-    <footer className="mb-5 mt-5 border-top pt-5">
-      <Copyright
-        serviceName="Shifter"
-        today={new Date('2010-01-01T00:00:00.000Z')}
-      />
+		<footer className="mb-5 mt-5 border-top pt-5">
+			<p>Copyright © yotigory. All rights reserved.</p>
     </footer>
   );
 };
--- apps/task/src/app/page/login.tsx
+++ apps/task/src/app/page/login.tsx
@@ -1,103 +1,98 @@
-import { useState } from "react";
-import { Container, Row, Col, } from 'reactstrap';
+import { useState,useEffect,useCallback } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import { FaUser, FaLock } from 'react-icons/fa';
 import {
   Layouts,
-  ImageLoginInformation,
   FormField,
   AuthFormHeader,
   ImageShifterLogo,
-	Checkbox,
-	Button
+  Link,
+  Button,
 } from '@galaxy/shifter';
 import { Footer } from '../component/footer';
+import '../css/style.css';
 
 export const Login = () => {
-	const [check, setCheck] = useState(false);
-	const [form, setForm] = useState({ password: '', email: ''});
-	const handleChange = (input:any) => (e:any) => {
-		setForm({...form, [input] : e.target.value});
-	};
-	const checkChange = () => (e:any) => {
-		setCheck(e.target.checked);
-};
+	const [status, setStatus] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+  const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [canSubmit, setCanSubmit] = useState(false);
+	useEffect(() => {
+    if (!username || !password) {
+      setCanSubmit(false);
+    } else {
+      setCanSubmit(true);
+    }
+	}, [setCanSubmit, username, password]);
+	const handleLogin: React.FormEventHandler<HTMLFormElement> = useCallback(
+    async (e) => {
+      e.preventDefault();
+      setStatus('inprogress');
+      try {
+        await handleSignIn(username, password, dispatchAuthStateChangeEvent);
+      } catch (e) {
+        setErrorMessage(e.message);
+        setStatus('failure');
+      }
+    },
+    [username, password, setStatus, setErrorMessage]
+  );
 
   return (
-    <>
-      <Layouts variant="login">
-        <Container className="themed-container">
-          <Row className="d-flex justify-content-between align-items-center">
-            <Col lg="5">
-              <div className="bg-white shadow-sm text-dark bg-white rounded pt-4 pr-4 pb-2 pl-4">
-                <AuthFormHeader
-                  link="/"
-                  linkText="sign up"
-                  logo={<ImageShifterLogo height="46" width="36" />}
-                  title="Log in"
-                  variant="signup"
-                />
-								<FormField
-									errorMessage=""
-									icon={<FaUser className="d-block" />}
-									id="email"
-									label="Username or email"
-									onChange={handleChange('email')}
-									type="text"
-									value={ form.email }
-                />
-                <FormField
-									errorMessage=""
-									icon={<FaLock className="d-block" />}
-									id="password"
-									label="password"
-									onChange={handleChange('password')}
-									type="password"
-									value={ form.password }
-                />
-                <Checkbox
-                  id="remember"
-                  label="Remember me"
-									onChange={checkChange()}
-									value={ check }
-								/>
-                <Button
-                  className="mt-4 mb-5 btn-primary"
-                  block
-                  fontWeight="bold"
-									textAlign="center"
-                >
-                  Log in
-								</Button>
-                <p>
-                  <a href="">Forgot Password?</a>
-                </p>
-              </div>
-            </Col>
-            <Col lg="5" className="text-center pt-5 pb-5">
-              <ImageLoginInformation
-                style={{
-                  margin: '0 0 5.4375rem 0',
-                }}
-                width="153px"
+    <Layouts variant="login">
+      <Container className="themed-container">
+        <Row className="d-flex justify-content-between align-items-center">
+					<Col lg="5">
+					<form autoComplete="on" noValidate onSubmit={handleLogin}>
+            <div className="bg-white shadow-sm text-dark bg-white rounded pt-4 pr-4 pb-2 pl-4">
+              <AuthFormHeader
+                link="/"
+                linkText="sign up"
+                logo={<ImageShifterLogo height="46" width="36" />}
+                title="Log in"
+                variant="signup"
               />
-              <h2 className="login-information-title text-primary">
-                Faster Build Times
-              </h2>
-              <p className="mt-4 mb-5 h5">
-                Customize Shifter settings for up to 70% faster build times.
-              </p>
-              <p className="font-weight-bold">
-                <a href="">
-                  Learn about Shifter Media CDN
-                </a>
-              </p>
-            </Col>
-          </Row>
-				</Container>
-				<div className="fixed-bottom">
+              <FormField
+                errorMessage=""
+                icon={<FaUser className="d-block" />}
+                id="email"
+                label="Username or email"
+                onChange={({ target: { value } }) => setUsername(value)}
+                type="text"
+                value={username}
+              />
+              <FormField
+                errorMessage=""
+                icon={<FaLock className="d-block" />}
+                id="password"
+                label="password"
+                onChange={({ target: { value } }) => setPassword(value)}
+                type="password"
+                value={password}
+              />
+              <Button
+                type="submit"
+                className="mt-4 mb-5 btn-primary"
+                block
+                fontWeight="bold"
+                textAlign="center"
+                disabled={!canSubmit}
+              >
+                {status === 'inprogress' ? 'Loading...' : 'Log In'}
+              </Button>
+              <div className="forget-password">
+                <Link href="/forget_password">{'Forgot Password?'}</Link>
+              </div>
+						</div>
+					</form>
+          </Col>
+          <Col lg="5" className="text-center pt-5 pb-5"></Col>
+        </Row>
+      </Container>
+      <div className="fixed-bottom">
         <Footer></Footer>
-				</div>
-      </Layouts>
-    </>
+      </div>
+    </Layouts>
   );
 };
